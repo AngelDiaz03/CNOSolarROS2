cfc4ec17e1b69ad8c07d9227f841ed4c30e767a4
AngelDiaz03 <manelonx@gmail.com>
2023-06-05 11:21:26 -0500

    style corrections

--- src/meteorology_data/meteorology_data/solar_park_node.py
+++ src/meteorology_data/meteorology_data/solar_park_node.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
 from interfaces.msg import Timestamp
-import cnsolar as cno 
+import cnsolar as cno
 import pandas as pd
 import json
 
@@ -11,20 +12,37 @@ class Solar_park(Node):
 
     def __init__(self):
         super().__init__("solar_park_node")
-        self.GHI_subscriber = self.create_subscription(Float64,"GHI_msg",self.GHI_callback,10)
-        self.POA_subscriber = self.create_subscription(Float64,"POA_msg",self.POA_callback,10)
-        self.tamb_subscriber = self.create_subscription(Float64,"tamb_msg",self.tamb_callback,10)
-        self.tmod_subscriber = self.create_subscription(Float64,"tmod_msg",self.tmod_callback,10)
-        self.time_subscriber = self.create_subscription(Timestamp,"time_msg",self.time_callback,10)
-
-        self.power_ac_msg = self.create_publisher(Float64,"power_ac",10)
-        self.power_dc_msg = self.create_publisher(Float64,"power_dc",10)
-
-
+        # Subscribers
+        self.GHI_subscriber = self.create_subscription(Float64,
+                                                       "GHI_msg",
+                                                       self.GHI_callback,
+                                                       10)
+        self.POA_subscriber = self.create_subscription(Float64,
+                                                       "POA_msg",
+                                                       self.POA_callback,
+                                                       10)
+        self.tamb_subscriber = self.create_subscription(Float64,
+                                                        "tamb_msg",
+                                                        self.tamb_callback,
+                                                        10)
+        self.tmod_subscriber = self.create_subscription(Float64,
+                                                        "tmod_msg",
+                                                        self.tmod_callback,
+                                                        10)
+        self.time_subscriber = self.create_subscription(Timestamp,
+                                                        "time_msg",
+                                                        self.time_callback,
+                                                        10)
+        # Publishers
+        self.power_ac_msg = self.create_publisher(Float64,
+                                                  "power_ac",
+                                                  10)
+        self.power_dc_msg = self.create_publisher(Float64,
+                                                  "power_dc",
+                                                  10)
+        # Initialization
         self.get_logger().info("solar park node started")
-
         self.create_timer(0.1, self.test_callback)
-        
         with open('src/cnsolar/resource/system_config_sd_51.json') as f:
             self.json_str = json.load(f)
         self.system_configuration = {}
@@ -35,17 +53,24 @@ class Solar_park(Node):
         self.time_flag = False 
 
     def test_callback(self):
-        if self.GHI_flag and self.POA_flag and self.tamb_flag and self.tmod_flag and self.time_flag:
+        if self.GHI_flag and self.POA_flag and \
+                             self.tamb_flag and \
+                             self.tmod_flag and \
+                             self.time_flag:
+            
             self.system_configuration[0] = self.json_str
             data = {'GHI': [self.GHI],
-            'Tamb': [self.tamb],
-            'Effective_Irradiance': [self.POA],
-            'Tmod': [self.tmod]}
+                    'Tamb': [self.tamb],
+                    'Effective_Irradiance': [self.POA],
+                    'Tmod': [self.tmod]}
             index = pd.DatetimeIndex([self.timestamp], tz='UTC-05:00')
             df = pd.DataFrame(data, index=index)
             availability = None
             energy_units = 'Wh'
-            self.output_data = cno.pipeline.run(self.system_configuration, df, availability, energy_units)
+            self.output_data = cno.pipeline.run(self.system_configuration,
+                                                df,
+                                                availability,
+                                                energy_units)
             self.get_logger().info(str(self.output_data['plant']))
             self.send_power_dc_data()
             self.send_power_ac_data()
@@ -77,13 +102,16 @@ class Solar_park(Node):
         self.time_flag = True
 
     def send_power_dc_data(self):
+        # Publish power_dc data
         msg = Float64()
         msg.data = self.output_data['plant']['p_dc'].values[0]
         self.power_dc_msg.publish(msg)
+
     def send_power_ac_data(self):
+        # Publish power_ac data
         msg = Float64()
         msg.data = self.output_data['plant']['ac'].values[0]
-        self.power_ac_msg.publish(msg) 
+        self.power_ac_msg.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
